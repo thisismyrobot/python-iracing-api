abe138f36cb75680ca5411a2a4d2738e3d70b7ea
Robert <rwallhead@gmail.com>
2013-03-11 16:02:28 +1100

    Now programatically generating more data, and applied the cached property pattern further

--- api.py
+++ api.py
@@ -5,116 +5,27 @@ import yaml # Requires PyYAML
 
 MEMMAPFILE = 'Local\\IRSDKMemMapFileName'
 
-TELEM_HEADER_LEN = 144
-TELEM_HEADER_SIZE = 28
+HEADER_LEN = 144
 
 # How far into a header the name sits, and it's max length
 TELEM_NAME_OFFSET = 16
 TELEM_NAME_MAX_LEN = 32
 
+# There appears to be triple-buffering of data
 VAL_BUFFERS = 3
 
+# The mapping between the type integer in memory mapped file and Python's struct
 TYPEMAP = ['c', '?', 'i', 'I', 'f', 'd']
-
-VALOFFSETS = {
-    'SessionTime':0,
-    'SessionNum':8,
-    'SessionState':12,
-    'SessionUniqueID':16,
-    'SessionFlags':20,
-    'SessionTimeRemain':24,
-    'SessionLapsRemain':32,
-    'RadioTransmitCarIdx':36,
-    'DriverMarker':40,
-    'IsReplayPlaying':41,
-    'ReplayFrameNum':42,
-    'ReplayFrameNumEnd':46,
-    'CarIdxLap':50,
-    'CarIdxLapDistPct':306,
-    'CarIdxTrackSurface':562,
-    'CarIdxOnPitRoad':818,
-    'OnPitRoad':882,
-    'CarIdxSteer':883,
-    'CarIdxRPM':1139,
-    'CarIdxGear':1395,
-    'SteeringWheelAngle':1651,
-    'Throttle':1655,
-    'Brake':1659,
-    'Clutch':1663,
-    'Gear':1667,
-    'RPM':1671,
-    'Lap':1675,
-    'LapDist':1679,
-    'LapDistPct':1683,
-    'RaceLaps':1687,
-    'LapBestLap':1691,
-    'LapBestLapTime':1695,
-    'LapLastLapTime':1699,
-    'LapCurrentLapTime':1703,
-    'LapDeltaToBestLap':1707,
-    'LapDeltaToBestLap_DD':1711,
-    'LapDeltaToBestLap_OK':1715,
-    'LapDeltaToOptimalLap':1716,
-    'LapDeltaToOptimalLap_DD':1720,
-    'LapDeltaToOptimalLap_OK':1724,
-    'LapDeltaToSessionBestLap':1725,
-    'LapDeltaToSessionBestLap_DD':1729,
-    'LapDeltaToSessionBestLap_OK':1733,
-    'LapDeltaToSessionOptimalLap':1734,
-    'LapDeltaToSessionOptimalLap_DD':1738,
-    'LapDeltaToSessionOptimalLap_OK':1742,
-    'LongAccel':1743,
-    'LatAccel':1747,
-    'VertAccel':1751,
-    'RollRate':1755,
-    'PitchRate':1759,
-    'YawRate':1763,
-    'Speed':1767,
-    'VelocityX':1771,
-    'VelocityY':1775,
-    'VelocityZ':1779,
-    'Yaw':1783,
-    'Pitch':1787,
-    'Roll':1791,
-    'PitRepairLeft':1795,
-    'PitOptRepairLeft':1799,
-    'CamCarIdx':1803,
-    'CamCameraNumber':1807,
-    'CamGroupNumber':1811,
-    'CamCameraState':1815,
-    'IsOnTrack':1819,
-    'IsInGarage':1820,
-    'SteeringWheelTorque':1821,
-    'SteeringWheelPctTorque':1825,
-    'ShiftIndicatorPct':1829,
-    'ShiftPowerPct':1833,
-    'ShiftGrindRPM':1837,
-    'EngineWarnings':1841,
-    'FuelLevel':1845,
-    'FuelLevelPct':1849,
-    'ReplayPlaySpeed':1853,
-    'ReplayPlaySlowMotion':1857,
-    'ReplaySessionTime':1858,
-    'ReplaySessionNum':1866,
-    'WaterTemp':1870,
-    'WaterLevel':1874,
-    'FuelPress':1878,
-    'OilTemp':1882,
-    'OilPress':1886,
-    'OilLevel':1890,
-    'Voltage':1894,
-    'ManifoldPress':1898,
-    'RRshockDefl':1902,
-    'LRshockDefl':1906,
-    'RFshockDefl':1910,
-    'LFshockDefl':1914,
-}
-
 
 class API(object):
 
     def __init__(self):
-        self._var_types = self._buffer_offsets = self._sizes = self._mmp = None
+        self._var_types = None
+        self._buffer_offsets = None
+        self._sizes = None
+        self._mmp = None
+        self._var_offsets = None
+        self._names = None
 
     @property
     def _telemetry_header_start(self):
@@ -141,6 +52,14 @@ class API(object):
             else:
                 offset += len(line)
         return offset + len(headers) + 4
+
+    def _get(self, position, type):
+        """ Gets a value from the mmp, based on a position and struct var type.
+        """
+        size = struct.calcsize(type)
+        self.mmp.seek(position)
+        data = self.mmp.read(size)
+        return struct.unpack(type, data)[0]
 
     @property
     def mmp(self):
@@ -171,14 +90,29 @@ class API(object):
         """ Find the offsets for the value array(s), cache the result.
         """
         if self._buffer_offsets is None:
-            self._buffer_offsets = []
-            for i in range(VAL_BUFFERS):
-                seek = 52 + (i * 16)
-                self.mmp.seek(seek)
-                data = self.mmp.read(4)
-                offset = struct.unpack('i', data)[0]
-                self._buffer_offsets.append(offset)
+            self._buffer_offsets = [self._get(52 + (i * 16), 'i')
+                                    for i
+                                    in range(VAL_BUFFERS)]
         return self._buffer_offsets
+
+    @property
+    def names(self):
+        """ The names of the telemetry variables, in order, cached.
+            TODO: Make less clunky...
+        """
+        if self._names is None:
+            self._names = []
+            self.mmp.seek(self._telemetry_header_start)
+            while True:
+                pos = self.mmp.tell() + TELEM_NAME_OFFSET
+                start = TELEM_NAME_OFFSET
+                end = TELEM_NAME_OFFSET + TELEM_NAME_MAX_LEN
+                header = self.mmp.read(HEADER_LEN)
+                name = header[start:end].replace('\x00','')
+                if name == '':
+                    break
+                self._names.append(name)
+        return self._names
 
     @property
     def var_types(self):
@@ -186,18 +120,23 @@ class API(object):
         """
         if self._var_types is None:
             self._var_types = {}
-            self.mmp.seek(self._telemetry_header_start)
-            while True:
-                pos = self.mmp.tell() + TELEM_NAME_OFFSET
-                start = TELEM_NAME_OFFSET
-                end = TELEM_NAME_OFFSET + TELEM_NAME_MAX_LEN
-                header = self.mmp.read(TELEM_HEADER_LEN)
-                name = header[start:end].replace('\x00','')
-                if name == '':
-                    break
-                var_type = TYPEMAP[int(struct.unpack('i', header[0:4])[0])]
-                self._var_types[name] = var_type
+            for i, name in enumerate(self.names):
+                type_loc = self._telemetry_header_start + (i * HEADER_LEN)
+                self._var_types[name] = TYPEMAP[int(self._get(type_loc, 'i'))]
         return self._var_types
+
+    @property
+    def var_offsets(self):
+        """ Find the offsets between the variables - used to find values in real
+            time. Results are cached.
+        """
+        if self._var_offsets is None:
+            self._var_offsets = {}
+            offsets_seek = self._get(28, 'i')
+            for i, name in enumerate(self.names):
+                offset = self._get(offsets_seek + (i * HEADER_LEN) + 4, 'i')
+                self._var_offsets[name] = offset
+        return self._var_offsets
 
     def telemetry(self, key):
         """ Return the data for a telemetry key.
@@ -231,7 +170,8 @@ if __name__ == '__main__':
     """
     import time
     api = API()
-    
+    api.var_offsets
+
     while True:
         print '{0} Gear, {1} m/s, {2} m/s/s'.format(api.telemetry('Gear'),
                                                     api.telemetry('Speed'),
